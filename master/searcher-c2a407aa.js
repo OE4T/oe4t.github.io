0798f9d00fdbb8c741fe0c5ebe7f6e9446b58bfa
dwalkes <dwalkes@users.noreply.github.com>
2026-03-07 13:19:19 +0000

    deploy: Trellis-Logic/meta-tegra@b34d36da6f9fe30b6160d3dc06406d5173ec194f

--- master/searcher-c2a407aa.js
+++ master/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bbc79546.js',
+                path_to_root + 'searchindex-60b6aa07.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
