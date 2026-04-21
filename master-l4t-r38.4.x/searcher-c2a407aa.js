e33930851fc3e44828c9af8c41bac3756973dec5
madisongh <madisongh@users.noreply.github.com>
2026-04-21 17:38:02 +0000

    deploy: OE4T/meta-tegra@1f28e68deca6773059da8635f0353aff23340790

--- master-l4t-r38.4.x/searcher-c2a407aa.js
+++ master-l4t-r38.4.x/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a800b9b7.js',
+                path_to_root + 'searchindex-dff33d09.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
