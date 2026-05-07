4f6d5b60f7be8b751fd8f9ff306c7bd86294eaaa
madisongh <madisongh@users.noreply.github.com>
2026-05-07 19:43:04 +0000

    deploy: OE4T/meta-tegra@e42f244f87f34dc8b224e9d86250159e2fe9acb2

--- scarthgap/searcher-c2a407aa.js
+++ scarthgap/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e46c8b48.js',
+                path_to_root + 'searchindex-39e8e2ee.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
