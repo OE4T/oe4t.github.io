f527d5576baedf3a68bb59e7aff7beb1bee935d8
madisongh <madisongh@users.noreply.github.com>
2026-07-26 12:26:36 +0000

    deploy: OE4T/meta-tegra@545e70e8c61094a06f65b1ac7dbebfa950e2ee68

--- wrynose/searcher-09f2665d.js
+++ wrynose/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-34ee0d4a.js',
+                path_to_root + 'searchindex-b4ad4e66.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
